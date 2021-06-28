5f820cc6fd9bcf79bf396ad72aa96dfcbe68feeb
Dev-Mehta <devbmehta04@gmail.com>
2021-06-28 16:44:38 +0530

    More settings for production

--- blossom/settings.py
+++ blossom/settings.py
@@ -125,6 +125,8 @@ STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'static'
 MEDIA_ROOT = BASE_DIR / 'media'
 MEDIA_URL = '/media/'
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
